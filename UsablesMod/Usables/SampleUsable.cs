83cd6f83c4cca72b65b9d002eb30a8fe774daec4
Ofir Cohen <ofirgvc@gmail.com>
2021-08-06 18:51:46 +0300

    Added incomplete UsablesMod source code

---
+++ UsablesMod/Usables/SampleUsable.cs
@@ -0,0 +1,12 @@
+﻿using static RandomizerMod.LogHelper;
+
+namespace UsablesMod.Usables
+{
+    class SampleUsable
+    {
+        public static void Run()
+        {
+            Log("Running SampleUsable");
+        }
+    }
+}
---
+++ UsablesMod/UsablesMod.cs
@@ -0,0 +1,51 @@
+﻿using Modding;
+using System;
+using UsablesMod.Usables;
+
+namespace UsablesMod
+{
+    public class UsablesMod : Mod, ITogglableMod
+    {
+        public UsablesMod Instance { get; private set; }
+
+		public override void Initialize()
+		{
+			if (Instance != null)
+			{
+				LogWarn("Initialized twice... Stop that.");
+				return;
+			}
+
+			Instance = this;
+
+			RandomizerMod.GiveItemActions.ExternItemHandlers.Add(TriggerUsable);
+			RandomizerMod.Randomization.PostRandomizer.PostRandomizationActions += ReplaceKPGeoRock;
+		}
+
+        private static bool TriggerUsable(RandomizerMod.GiveItemActions.GiveAction action,
+			string item, string location, int geo)
+        {
+			SampleUsable.Run();
+
+			return false;
+        }
+
+        private void ReplaceKPGeoRock()
+        {
+            string kingsPassLeftGeoRockLocation = "Geo_Rock-King's_Pass_Left";
+            RandomizerMod.RandomizerMod.Instance.Settings.AddItemPlacement("Rancid_Egg-Weaver's_Den", kingsPassLeftGeoRockLocation);
+        }
+
+        public override string GetVersion()
+        {
+            string ver = "0.0.1";
+            return ver;
+        }
+
+        public void Unload()
+        {
+			Instance = null;
+			RandomizerMod.GiveItemActions.ExternItemHandlers.Remove(TriggerUsable);
+		}
+    }
+}
