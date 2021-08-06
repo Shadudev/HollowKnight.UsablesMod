c930235ef7a75d0e162a9cbb6b2638ead486aec3
Ofir Cohen <ofirgvc@gmail.com>
2021-08-06 22:04:22 +0300

    Made geo rock to the left of king's pass to become a shiny (mock-randomized check)

--- UsablesMod/UsablesMod.cs
+++ UsablesMod/UsablesMod.cs
@@ -8,19 +8,27 @@ namespace UsablesMod
     {
         public UsablesMod Instance { get; private set; }
 
-		public override void Initialize()
-		{
-			if (Instance != null)
-			{
-				LogWarn("Initialized twice... Stop that.");
-				return;
-			}
+        public override void Initialize()
+        {
+            if (Instance != null)
+            {
+                LogWarn("Initialized twice... Stop that.");
+                return;
+            }
 
-			Instance = this;
+            Instance = this;
 
-			RandomizerMod.GiveItemActions.ExternItemHandlers.Add(TriggerUsable);
-			RandomizerMod.Randomization.PostRandomizer.PostRandomizationActions += ReplaceKPGeoRock;
-		}
+            AddKpRockCheck();
+            RandomizerMod.GiveItemActions.ExternItemHandlers.Add(TriggerUsable);
+        }
+
+        private static void AddKpRockCheck() {
+            RandomizerMod.Randomization.PostRandomizer.PostRandomizationActions -=
+                RandomizerMod.Randomization.PostRandomizer.CreateActions;
+            RandomizerMod.Randomization.PostRandomizer.PostRandomizationActions += ReplaceKPGeoRock;
+            RandomizerMod.Randomization.PostRandomizer.PostRandomizationActions +=
+                RandomizerMod.Randomization.PostRandomizer.CreateActions;
+        }
 
         private static bool TriggerUsable(RandomizerMod.GiveItemActions.GiveAction action,
 			string item, string location, int geo)
@@ -30,7 +38,7 @@ namespace UsablesMod
 			return false;
         }
 
-        private void ReplaceKPGeoRock()
+        private static void ReplaceKPGeoRock()
         {
             string kingsPassLeftGeoRockLocation = "Geo_Rock-King's_Pass_Left";
             RandomizerMod.RandomizerMod.Instance.Settings.AddItemPlacement("Rancid_Egg-Weaver's_Den", kingsPassLeftGeoRockLocation);
