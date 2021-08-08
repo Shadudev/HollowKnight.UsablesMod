afd80b02987df11cce4b4f30df99d71624d40863
schyvun <julisullek@gmx.de>
2021-08-08 18:32:35 +0200

    4 new usables

---
+++ UsablesMod/Usables/MPCostUsable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UsablesMod.Usables
+{
+    class MPCostUsable : IUsable
+    {
+        public void Run()
+        {
+            PlayMakerFSM SpellControl = HeroController.instance.gameObject.LocateMyFSM("Spell Control");
+            SpellControl.FsmVariables.FindFsmInt("MP Cost").Value = 66;
+        }
+
+        public bool IsRevertable()
+        {
+            return true;
+        }
+
+        public float GetDuration()
+        {
+            return 30;
+        }
+
+        public void Revert()
+        {
+            PlayMakerFSM SpellControl = HeroController.instance.gameObject.LocateMyFSM("Spell Control");
+            SpellControl.FsmVariables.FindFsmInt("MP Cost").Value = 33;
+        }
+
+        public string GetName()
+        {
+            return "MPCost";
+        }
+    }
+}
