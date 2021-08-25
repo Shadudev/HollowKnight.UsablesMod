f250adc3ea7f09fabfffac76ce0f685313874646
schyvun <julisullek@gmx.de>
2021-08-25 15:50:44 +0200

    dmg usable

---
+++ UsablesMod/UsablesManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UsablesMod.Usables;
+
+namespace UsablesMod
+{
+    class UsablesManager : MonoBehaviour
+    {
+        public void Run(string usableName)
+        {
+            IUsable usable = GetUsable(usableName);
+            RunUsable(usable);
+        }
+
+        private IUsable GetUsable(string usableName)
+        {
+            switch (usableName)
+            {
+                case "Rancid_Egg-Weaver's_Den":
+                    return new HealOrDmgUsable();
+                default:
+                    return new SampleUsable();
+            }
+        }
+
+        private void RunUsable(IUsable usable)
+        {
+            GameManager.instance.StartCoroutine(RunUsableRoutine(usable));
+        }
+
+        private IEnumerator RunUsableRoutine(IUsable usable)
+        {
+            LogHelper.Log($"Running {usable.GetName()} routine");
+            usable.Run();
+            if (usable.IsRevertable())
+            {
+                float duration = usable.GetDuration();
+                LogHelper.Log($"Reverting {usable.GetName()} in {duration}"); 
+                yield return new WaitForSeconds(duration);
+                usable.Revert();
+            }
+        }
+    }
+}
