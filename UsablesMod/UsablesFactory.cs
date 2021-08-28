8c9353243a465605544cd3ade703cb128f335815
schyvun <julisullek@gmx.de>
2021-08-28 21:21:21 +0200

    Removed Movememnt Usable

--- UsablesMod/UsablesFactory.cs
+++ UsablesMod/UsablesFactory.cs
@@ -50,8 +50,6 @@ namespace UsablesMod
                     return new BounceUsable(randomSeed);
                 case 9:
                     return new RandomCharmsUsable(randomSeed);
-                case 10:
-                    return new MovementUsable(randomSeed);
                 default:
                     return new SampleUsable(randomSeed);
             }
